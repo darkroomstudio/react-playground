8457bf9b03552aa3f1bcea83f9522a67281475b3
George Kim <ggkim0614@gmail.com>
2024-03-07 00:47:25 -0800

    ADD: add scrolltriggerpage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import * as pg from './playground';
+import './App.css';
+
 export default function App() {
-  return null
+	return (
+		<Router>
+			Home
+			<Link to="/scroll-trigger">GSAP Scroll Trigger Example</Link>
+			<Routes>
+				<Route path="/" element={<pg.ScrollTriggerPage />}></Route>
+				<Route
+					path="/scroll-trigger"
+					element={<pg.ScrollTriggerPage />}
+				></Route>
+			</Routes>
+		</Router>
+	);
 }
